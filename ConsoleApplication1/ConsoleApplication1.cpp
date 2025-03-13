1dc302dd01d21eeb04969eb4ac1adea64d1dd757
Gifotto <nina.tedikova.07@bk.ru>
2025-03-14 02:57:36 +0700

    Init commit_____adding a project to the git

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,38 @@
+﻿/*
+1.	Геометрические фигуры. Реализовать функции вычисления параметров для двух-трех следующих геометрических фигур (по одной фигуре на студента):
+	a.	Прямоугольник – периметр, площадь, длина диагонали;
+*/
+
+#include <iostream>
+#include <Windows.h>
+#include <cmath>
+
+using namespace std;
+
+int FuncRectangle(int a, int b) {
+	enum rec {P, S, D};
+	int PSD, vib;
+	cout << "Периметр — 0\nПлощадь — 1\nДиагональ — 2\n";
+	cin >> vib;
+	if (vib == rec::P) {
+		PSD = 2 * (a + b);
+	}
+	else if (vib == rec::S) {
+		PSD = a * b;
+	}
+	else if (vib == rec::D) {
+		PSD = sqrt(a*a+b*b);
+	}
+	return PSD;
+}
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int a, b;
+	cout << "Введите стороны прямоугольника: ";
+	cin >> a >> b;
+	cout << FuncRectangle(a, b);
+}
+
